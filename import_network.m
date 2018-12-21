c30db8f43b77e3ae33ad1e32e438f2c81cc383c8
robertmarsland <robertvsiii@gmail.com>
2018-12-21 15:47:10 -0500

    network plots

--- import_network.m
+++ import_network.m
@@ -2,8 +2,11 @@
 thresh = 1e-2;
 
 %% Import the data
-%% fluxes_23.xlsx is the resource-limited example, while fluxes_90.xlsx is the diverse example
-[~, ~, raw] = xlsread('fluxes_90.xlsx','Sheet1');
+%% fluxes_23_main_dataset.xlsx is the resource-limited example, 
+%% while fluxes_90_main_dataset.xlsx is the diverse example. Changing
+%% the suffix to 'dense_metabolism' loads the networks for the dense
+%% metabolic matrix
+[~, ~, raw] = xlsread('fluxes_90_dense_metabolism.xlsx','Sheet1');
 raw = raw(4:end,3:end);
 
 %% Create output variable
@@ -17,4 +20,25 @@ G = digraph(fluxes');
 clearvars raw;
 
 %% Plot the graph
-plot(G,'LineWidth',10*G.Edges.Weight/max(G.Edges.Weight),'Layout','layered','Sources',[1])
+fig = figure;
+h = plot(G,'LineWidth',10*G.Edges.Weight/max(G.Edges.Weight),'Layout','layered','Sources',[1],'NodeLabel',{});
+print(fig,'Network_90_dense','-depsc')
+
+%% Do it again
+[~, ~, raw] = xlsread('fluxes_23_dense_metabolism.xlsx','Sheet1');
+raw = raw(4:end,3:end);
+
+%% Create output variable
+fluxes = reshape([raw{:}],size(raw));
+
+fluxes((fluxes/max(max(fluxes)))<thresh) = 0;
+
+G = digraph(fluxes');
+
+%% Clear temporary variables
+clearvars raw;
+
+%% Plot the graph
+fig = figure;
+h = plot(G,'LineWidth',10*G.Edges.Weight/max(G.Edges.Weight),'Layout','layered','Sources',[1],'NodeLabel',{});
+print(fig,'Network_23_dense','-depsc')
